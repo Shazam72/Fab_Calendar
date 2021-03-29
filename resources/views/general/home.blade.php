12fd1e80b909ac1c0f1cee51c4d959ed30a0513b
JfrancoisZouba <jfrancoiszouba72@gmail.com>
2021-03-29 10:22:35 +0000

    --amend

--- resources/views/general/home.blade.php
+++ resources/views/general/home.blade.php
@@ -5,9 +5,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/fonts/css/all.css">
-    <link rel="stylesheet" href="/css/connect.css">
+    <link rel="stylesheet" href="{{ secure_asset('css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{ secure_asset('fonts/css/all.css')}}">
+    <link rel="stylesheet" href="{{ secure_asset('css/connect.css')}}">
     <title>Accueil - Fab Calendar</title>
 </head>
 
@@ -47,7 +47,7 @@
         </section>
     </div>
 
-    <script src="{{ asset('js/jquery-3.5.1.min.js')}}"></script>
+    <script src="{{ secure_asset('js/jquery-3.5.1.min.js')}}"></script>
     <script>
         $('header button').click(function() {
             window.location.href = this.dataset.target;
