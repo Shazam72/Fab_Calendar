9ba2606e21156764909cadc5d4337d8c301f50ae
JfrancoisZouba <jfrancoiszouba72@gmail.com>
2021-03-29 12:12:28 +0000

    --amend

--- resources/views/general/layout.blade.php
+++ resources/views/general/layout.blade.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ secure_asset('css/bootstrap.min.css')}}">
-    <link rel="stylesheet" href="{{ secure_asset('fonts/css/all.css')}}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
     <link rel="stylesheet" href="{{ secure_asset('css/layout.css')}}">
     @yield('style_page')
     <title>@yield('title_page','Fab Calendar')</title>
@@ -212,9 +212,9 @@
             </div>
         </footer>
     </div>
-    <script src="{{ secure_asset('js/popper.min.js')}}"></script>
     <script src="{{ secure_asset('js/jquery-3.5.1.min.js')}}"></script>
-    <script src="{{ secure_asset('js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{ secure_asset('js/popper.min.js')}}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
     <script src="{{ secure_asset('js/sweetalert.min.js')}}"></script>
     <script src="{{ secure_asset('js/layout.js')}}"></script>
     @yield('script')
